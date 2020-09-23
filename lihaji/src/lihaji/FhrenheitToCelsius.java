b274d4e73aa456053188078374c3d257af8bfe8e
lihahahaji <71313475+lihahahaji@users.noreply.github.com>
2020-09-23 14:01:18 +0800

    Add files via upload

---
+++ lihaji/src/lihaji/FhrenheitToCelsius.java
@@ -0,0 +1,13 @@
+package lihaji;
+import java.util.Scanner;
+
+public class FhrenheitToCelsius {
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a degree in Fahrenheit: ");
+		double fahrenheit = input.nextDouble();
+		double celsius = (5.0 / 9) * (fahrenheit - 32);
+		System.out.println("Fahrenheit "+fahrenheit+" is "+celsius+" in Celsius");
+	}
+}
---
+++ lihaji/src/lihaji/Welcome.java
@@ -0,0 +1,8 @@
+package lihaji;
+
+public class Welcome {
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Java!");
+	}
+}
